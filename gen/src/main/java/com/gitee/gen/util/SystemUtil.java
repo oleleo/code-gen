aacd1a8f28eebdf40041731481840d3d96e7854e
thc <8775@163.com>
2024-02-05 20:22:54 +0800

    2.0.0

--- gen/src/main/java/com/gitee/gen/util/SystemUtil.java
+++ gen/src/main/java/com/gitee/gen/util/SystemUtil.java
@@ -2,26 +2,37 @@ package com.gitee.gen.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @author tanghc
  */
 public class SystemUtil {
 
+    private static volatile String binPath;
+
     /**
      * 获取程序执行目录，即jar包所在的目录。此方法只在部署后有用，开发模式下，这里返回target路径
+     *
      * @return 返回路径
      */
     public static String getBinPath() {
-        String path = SystemUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        try {
-            path = URLDecoder.decode(path, "UTF-8");
-            path = StringUtil.trimTrailingCharacter(path, '/');
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
+        if (binPath == null) {
+            synchronized (SystemUtil.class) {
+                if (binPath == null) {
+                    String path = SystemUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+                    try {
+                        path = URLDecoder.decode(path, "UTF-8");
+                        path = StringUtil.trimTrailingCharacter(path, '/');
+                        int index = path.lastIndexOf('/');
+                        path = path.substring(0, index);
+                    } catch (UnsupportedEncodingException e) {
+                        throw new RuntimeException(e);
+                    }
+                    binPath = path;
+                }
+            }
         }
-        return path;
+        return binPath;
     }
 
     public static String getUserHome() {
