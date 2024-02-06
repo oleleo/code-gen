3164b6e728190d10466f0748fff598cd71ac4ac4
thc <8775@163.com>
2024-02-06 09:02:25 +0800

    2.0.0

--- gen/src/main/java/com/gitee/gen/controller/GenerateHistoryController.java
+++ gen/src/main/java/com/gitee/gen/controller/GenerateHistoryController.java
@@ -1,6 +1,6 @@
 package com.gitee.gen.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.gitee.gen.common.Action;
 import com.gitee.gen.common.GeneratorParam;
 import com.gitee.gen.common.Result;
