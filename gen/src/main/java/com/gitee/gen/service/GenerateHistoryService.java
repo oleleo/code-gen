30a390776ffa5a48b5281b7a001afe5550d701a7
thc <8775@163.com>
2024-02-06 09:07:53 +0800

    2.0.0

--- gen/src/main/java/com/gitee/gen/service/GenerateHistoryService.java
+++ gen/src/main/java/com/gitee/gen/service/GenerateHistoryService.java
@@ -1,6 +1,6 @@
 package com.gitee.gen.service;
 
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSON;
 import com.gitee.gen.common.GeneratorParam;
 import com.gitee.gen.entity.GenerateHistory;
 import com.gitee.gen.mapper.GenerateHistoryMapper;
